42ecbe54893b1ea95124c4d30aa103c1137de702
sugarrr <16698537+sugarrr57@user.noreply.gitee.com>
2026-03-20 22:24:29 +0800

    登录接口返参增加role

--- routes/auth.js
+++ routes/auth.js
@@ -120,6 +120,7 @@ router.post('/login', async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
+                role:user.role,
             },
         });
     } catch (error) {
